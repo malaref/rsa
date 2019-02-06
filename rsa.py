303101af92a1d9ec577929d2cdc58de1469242c4
muhammad-alaref <muhammad.alaref96@eng-st.cu.edu.eg>
2019-02-06 06:29:47 +0200

    Fixed ValueError and identical e,d bugs in generate_keys!

--- rsa.py
+++ rsa.py
@@ -21,16 +21,23 @@ def multiplicative_inverse(x, n):
             return i
 
 def generate_keys(key_size):
-    p = q = get_random_prime(key_size)
-    while q == p:
-        q = get_random_prime(key_size)
-    n = p * q
-    phi_n = (p-1) * (q-1)
-    while True:
-        e = randrange(2, phi_n)
-        if gcd(e, phi_n) == 1:
-            d = multiplicative_inverse(e, phi_n)
-            return e, d, n
+    if key_size < 3:
+        raise ValueError('Key size must not be < 3')
+    try:
+        p = q = get_random_prime(key_size)
+        while q == p:
+            q = get_random_prime(key_size)
+        n = p * q
+        phi_n = (p-1) * (q-1)
+        while True:
+            e = randrange(2, phi_n)
+            if gcd(e, phi_n) == 1:
+                d = multiplicative_inverse(e, phi_n)
+                if d == e:
+                    return generate_keys(key_size) # must choose new p and q; prettier than a huge loop
+                return e, d, n
+    except ValueError: # empty range for randrange()
+        return generate_keys(key_size) # since key_size is not < 3, this is guaranteed to terminate; just try again!
 
 def crypt(message, key):
     return message ** key[0] % key[1]
