13e47d0f0be4a98d8e2ecde981e88fb84ef14468
muhammad-alaref <muhammad.alaref96@eng-st.cu.edu.eg>
2019-02-06 08:53:59 +0200

    Minor refactoring; replacing key size with key length to better adhere to the doc!

--- cli.py
+++ cli.py
@@ -4,14 +4,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parsers = parser.add_subparsers(dest='command')
 generate_keys_parser = parsers.add_parser('generate_keys')
-generate_keys_parser.add_argument('key_size', type=int)
+generate_keys_parser.add_argument('key_length', type=int)
 crypt_parser = parsers.add_parser('crypt', aliases=['encrypt', 'decrypt'])
 crypt_parser.add_argument('key', type=int, nargs=2)
 crypt_parser.add_argument('message', type=int)
 args = parser.parse_args()
 
 if args.command == 'generate_keys':
-    print(generate_keys(args.key_size))
+    print(generate_keys(args.key_length))
 elif args.command == 'encrypt' or args.command == 'decrypt':
     print(crypt(args.message, args.key))
 else:
--- rsa.py
+++ rsa.py
@@ -9,9 +9,9 @@ def is_prime(number):
             return False
     return True
 
-def get_random_prime(size):
+def get_random_prime(length):
     while True:
-        number = getrandbits(size)
+        number = getrandbits(length)
         if is_prime(number):
             return number
 
@@ -28,13 +28,13 @@ def multiplicative_inverse(x, n): # using the extended Euclidean algorithm, sour
         t = t + n
     return t
 
-def generate_keys(key_size):
-    if key_size < 4:
-        raise ValueError('Key size must not be < 4')
+def generate_keys(key_length):
+    if key_length < 4:
+        raise ValueError('Key length must not be < 4')
     try:
-        p = q = get_random_prime(key_size)
+        p = q = get_random_prime(key_length)
         while q == p:
-            q = get_random_prime(key_size)
+            q = get_random_prime(key_length)
         n = p * q
         phi_n = (p-1) * (q-1)
         while True:
@@ -42,10 +42,10 @@ def generate_keys(key_size):
             if gcd(e, phi_n) == 1:
                 d = multiplicative_inverse(e, phi_n)
                 if d == e:
-                    return generate_keys(key_size) # must choose new p and q; prettier than a huge loop
+                    return generate_keys(key_length) # must choose new p and q; prettier than a huge loop
                 return e, d, n
     except ValueError: # empty range for randrange()
-        return generate_keys(key_size) # since key_size is not < 4, this is guaranteed to terminate; just try again!
+        return generate_keys(key_length) # since key_length is not < 4, this is guaranteed to terminate; just try again!
 
 def crypt(message, key):
     return message ** key[0] % key[1]
